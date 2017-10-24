05a56eb6a12778d2e915294272ea1dfe0807ad74
jkyle1 <gruneneune@gmail.com>
2017-10-24 15:06:42 +0100

    added static directory and blog.css; added base, detail and list templates; corrected errors in models.py get_absolute_url method and urls.py

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -9,7 +9,6 @@ import django.utils.timezone
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -27,8 +26,10 @@ class Migration(migrations.Migration):
                 ('publish', models.DateTimeField(default=django.utils.timezone.now)),
                 ('created', models.DateTimeField(auto_now_add=True)),
                 ('updated', models.DateTimeField(auto_now=True)),
-                ('status', models.CharField(choices=[('draft', 'Draft'), ('published', 'Published')], default='draft', max_length=10)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='blog_posts', to=settings.AUTH_USER_MODEL)),
+                ('status', models.CharField(choices=[('draft', 'Draft'), ('published', 'Published')], default='draft',
+                                            max_length=10)),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='blog_posts',
+                                             to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'ordering': ('-publish',),
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset().\
+        return super(PublishedManager, self).get_queryset(). \
             filter(status='published')
 
 
@@ -19,7 +20,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES,
-                              default='draft'),
+                              default='draft')
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
 
@@ -29,4 +30,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                self.publish.strftime('%m'),
+                                                self.publish.strftime('%d'),
+                                                self.slug])
